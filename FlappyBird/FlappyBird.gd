5eebf9527520e6b2c596321cc2b15182f9213665
Eric Alexander <alexandereric1999@gmail.com>
2023-10-31 21:16:13 -0400

    STABLE VERSION
    Dashing, Jumping implemented in a basic form. Start with ten jumps. Dashing gives you a jump.
    
    Does not die upon hitting bottom of the screen. Also does not die upon collision with pipes.

--- FlappyBird/FlappyBird.gd
+++ FlappyBird/FlappyBird.gd
@@ -1,24 +1,53 @@
 extends Node2D
 
+@export var column_spacing : float = 500
+@export var initial_opening_height : float = 100
+var current_opening_height : float = initial_opening_height
+var column_x : float
 var camera_instance = load("res://Main/camera_2d.tscn")
 var camera : Camera2D
+var camera_offset_from_player : float = 0
+var camera_center : Vector2
+var camera_size : Vector2
+var top_viewport_limit : float 
+var bot_viewport_limit : float
+
+var initialized : bool = false
+var obstacle_instance = load("res://FlappyBird/obstacle.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	camera = camera_instance.instantiate()
-	add_child(camera)
-	var camera_center = $Camera2D.get_screen_center_position()
-	var camera_size = $Camera2D.get_viewport().get_visible_rect().size
-	var top_limit = camera_center.y - camera_size.y/2
-	var bot_limit = camera_center.y + camera_size.y/2
-	$Player.set_position(camera_center*Vector2(0.33,1))
-	$Player.set_top_of_screen_y(top_limit)
-	var x_position = camera_center.x*2*3/4
-	var opening_height = 50
-	$Obstacle.create_obstacle(x_position,top_limit,bot_limit,opening_height)
-	pass # Replace with function body.
+	initialize_obstacles()
 
+func initialize_obstacles():
+	camera = $Camera2D
+	camera_center = camera.get_screen_center_position()
+	camera_size = camera.get_viewport().get_visible_rect().size
+	$Player.set_position(camera_center)
+	camera_offset_from_player = camera_size.x / 6
+	top_viewport_limit = camera_center.y - camera_size.y/2
+	bot_viewport_limit = camera_center.y + camera_size.y/2
+	var x_position = camera_center.x*2*3/4
+	$Player.set_top_of_screen_y(top_viewport_limit)
+	$Player.set_column_spacing(column_spacing)
+	column_x = $Player.get_position().x + column_spacing/2
+	initialized = true
+	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if initialized == true:
+		camera.set_position(Vector2($Player.get_position().x++camera_offset_from_player,camera.get_position().y))
+		camera_center = camera.get_screen_center_position()
+		while column_x <= camera_center.x + camera_size.x/2:
+
+			var obstacle = obstacle_instance.instantiate()
+			var opening_height = randf_range(current_opening_height,$Player/Collision.shape.radius*3)
+			obstacle.create_obstacle(column_x,top_viewport_limit,bot_viewport_limit,opening_height)
+			add_child(obstacle)
+			if current_opening_height > $Player/Collision.shape.radius*3:
+				current_opening_height = current_opening_height*.95
+			column_x += column_spacing
+			print(column_x)
 	pass
 
--- FlappyBird/Player.gd
+++ FlappyBird/Player.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 @export var color : Color
 var top_of_screen_y : float = -9999
-
+var column_spacing : float 
 
 @export var gravity : float = 15
 @export var jump_count : int = 1
 @export var jump_accelleration : float = -60
+@export var dash_speed : float = 500
 @export var max_velocity : float = 5000
 
 #Will cut all upward accelleration if it will take dampening_time in seconds to reach
@@ -20,10 +21,13 @@ var accelleration = Vector2.ZERO
 var spacebar_hold_time : float = 0
 const spacebar_max_hold_time : float = 0.5
 
+var dash_finished : Vector2
 
 func set_top_of_screen_y(input_y):
 		top_of_screen_y = input_y
-		
+
+func set_column_spacing(input_x):
+	column_spacing = input_x
 func _physics_process(delta):
 	if position.y <= top_of_screen_y:
 		position.y = top_of_screen_y
@@ -31,6 +35,21 @@ func _physics_process(delta):
 		accelleration.y = 0
 		spacebar_hold_time = 0
 		state = STATE.FALLING
+		
+	if Input.is_action_just_pressed("right") and state != STATE.DASHING:
+		state = STATE.DASHING
+		accelleration = Vector2.ZERO
+		velocity = Vector2(1,0)*dash_speed
+		spacebar_hold_time = 0
+		dash_finished = get_global_position()+Vector2(column_spacing,0)
+		jump_count += 1
+	if state == STATE.DASHING:
+		print(get_global_position().x>dash_finished.x)
+		
+	if state == STATE.DASHING and get_global_position().x >= dash_finished.x:
+		state = STATE.FALLING
+		velocity = Vector2.ZERO
+		
 	if Input.is_action_just_pressed("space_bar") and state == STATE.FALLING:
 		if jump_count > 0:
 
